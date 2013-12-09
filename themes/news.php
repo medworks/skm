4e36252d168e42c3c91ec19ee85ede256e0c4cfa
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-12-09 13:00:40 +0330

    fix news page

--- themes/news.php
+++ themes/news.php
@@ -4,54 +4,71 @@ $html=<<<cd
 	<div id="wrapper">
 		<div id="main_wrapper">
 			<div id="breadcrumb">
-				<a href="http://e404themes.com/cold">Home</a> <span>›</span>
-				<a href="http://e404themes.com/cold/blog/">Blog</a>
+				<a href="./">صفحه اصلی</a> <span>›</span>
+				<a>اخبار</a>
 			</div>
 			<div id="page-content" class="two_third">
 				<div id="post-174" class="post-174 post type-post status-publish format-standard sticky hentry category-news tag-design tag-works">
 					<div class="meta-date">
-						<span class="meta-month">Apr</span>
+						<span class="meta-month">فروردین</span>
 						<span class="meta-day">09</span>
-						<span class="meta-year">2011</span>
-						<div class="meta-comments">
-							<a href="http://e404themes.com/cold/2011/04/sample-blog-post/#comments"><span>0</span></a>
-						</div>
+						<span class="meta-year">1392</span>
+						<!-- <div class="meta-comments">
+							<a href="#"><span>0</span></a>
+						</div> -->
 					</div>
 					<h2 class="blog-header">
-						<a href="http://e404themes.com/cold/2011/04/sample-blog-post/" title="Sample sticky blog post">Sample sticky blog post</a>
+						<a href="#" title="">خبر اول</a>
 					</h2>
 					<div class="meta posted-meta">
-				 		by <a href="http://e404themes.com" title="Visit Bartosz Romanowski’s website" rel="author external">Bartosz Romanowski</a>
-				 		in <a href="http://e404themes.com/cold/category/news/" title="View all posts in News" rel="category tag">News</a>
+				 		به وسیله <a title="" rel="author external">مجتبی امجدی</a>
+				 		در گروه <a href="#" title="" rel="category tag">گیاهان دریایی</a>
 				 	</div>
-					<div class="meta tags-meta">
-						<a href="http://e404themes.com/cold/tag/design/" rel="tag">design</a>
-						<a href="http://e404themes.com/cold/tag/works/" rel="tag">works</a>
-					</div>
 					<br class="clear">
 					<div class="shadow shadow_huge aligncenter shadow_center">
-						<a href="http://e404themes.com/cold/wp-content/uploads/2011/04/img02.jpg" class="zoom" rel="prettyPhoto" title="">
-							<img src="http://e404themes.com/cold/wp-content/themes/cold/lib/timthumb.php?src=http://e404themes.com/cold/wp-content/uploads/2011/04/img02.jpg&amp;w=600&amp;h=0&amp;zc=1" alt="Sample sticky blog post" class="border-img">
+						<a href="#" class="zoom" rel="prettyPhoto" title="">
+							<img src="themes/images/demo/newspic.jpg" alt="" class="">
 						</a>
 					</div>
-					<p>Se ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+					<p>توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... </p>
 					<p class="more">
-						<a href="http://e404themes.com/cold/2011/04/sample-blog-post/">Read more</a>
+						<a href="#">ادامه خبر</a>
 					</p>
 				</div>
 				<div class="wp-pagenavi">
-					<span class="pages">Page 1 of 2</span><span class="current">1</span><a href="http://e404themes.com/cold/blog/page/2/" class="page larger">2</a><a href="http://e404themes.com/cold/blog/page/2/" class="nextpostslink">»</a>
+					<span class="pages">صفحه ۱ از ۲</span>
+					<span class="current">1</span>
+					<a href="#" class="page larger">2</a>
+					<a href="#" class="nextpostslink">&#8249;</a>
 				</div>
 			</div>
 			<div id="sidebar" class="one_third last">
 				<div id="search-2" class="widgets widget_search">
 					<form role="search" method="get" id="searchform" class="searchform" action="">
 						<div>
-							<label class="screen-reader-text" for="s">Search for:</label>
-							<input type="text" value="" name="s" id="s">
-							<input type="submit" id="searchsubmit" value="Search">
+							<label class="screen-reader-text" for="s">جستجو برای</label>
+							<input type="text" class="rtl" value="" name="s" id="s">
+							<input type="submit" id="searchsubmit" value="جستجو">
 						</div>
 					</form>
+				</div>
+        		<br class="clear">
+        		<div id="e404_twitter-2" class="widgets widget_twitter">
+        			<h3>آخرین اخبار</h3>
+					<ul class="tweets">
+						<li>
+							<a href="#" class="twitter-user">خبر یک</a>
+							<span>در 2 فروردین, ۱۳۹۲</a></span>
+						</li>
+						<li>
+							<a href="#" class="twitter-user">خبر یک</a>
+							<span>در 2 فروردین, ۱۳۹۲</a></span>
+						</li>
+						<li>
+							<a href="#" class="twitter-user">خبر یک</a>
+							<span>در 2 فروردین, ۱۳۹۲</a></span>
+						</li>						
+					</ul>
 				</div>
         		<br class="clear">
 			</div>
