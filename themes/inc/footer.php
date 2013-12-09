6c4fcd46dc8b51d868f41cc8344d202373eda92d
mojiAm <amjadi.mojtaba@gmail.com>
2013-12-08 19:45:40 -0800

    fix footer

--- themes/inc/footer.php
+++ themes/inc/footer.php
@@ -1,64 +1,79 @@
 <div id="footer_wrapper">
 	<div id="footer">
 		<div id="e404_flickr-3" class="one_fourth widgets widget_flickr">
-			<h3>Flickr Photos</h3>
+			<h3>گالری تصاویر</h3>
 			<ul class="flickr">
-				<li><a href="http://farm9.staticflickr.com/8498/8310480247_754c50e18d_b.jpg" rel="prettyphoto" title="DSC_0159"><img src="http://farm9.staticflickr.com/8498/8310480247_754c50e18d_s.jpg" width="50" height="50" alt="DSC_0159"></a>
+				<li>
+					<a href="#" rel="prettyphoto" title="DSC_0159"><img src="themes/images/demo/smallpic.jpg" width="50" height="50" alt="DSC_0159"></a>
 				</li>
-				<li><a href="http://farm9.staticflickr.com/8362/8311546462_a05f54117f_b.jpg" rel="prettyphoto" title="DSC_0103"><img src="http://farm9.staticflickr.com/8362/8311546462_a05f54117f_s.jpg" width="50" height="50" alt="DSC_0103"></a>
+				<li>
+					<a href="#" rel="prettyphoto" title="DSC_0103"><img src="themes/images/demo/smallpic.jpg" width="50" height="50" alt="DSC_0103"></a>
 				</li>
-				<li><a href="http://farm9.staticflickr.com/8362/8311536906_6f2bd7b25a_b.jpg" rel="prettyphoto" title="DSC_0126"><img src="http://farm9.staticflickr.com/8362/8311536906_6f2bd7b25a_s.jpg" width="50" height="50" alt="DSC_0126"></a>
+				<li>
+					<a href="#" rel="prettyphoto" title="DSC_0126"><img src="themes/images/demo/smallpic.jpg" width="50" height="50" alt="DSC_0126"></a>
 				</li>
-				<li><a href="http://farm9.staticflickr.com/8072/8311558088_ffc3fd6169_b.jpg" rel="prettyphoto" title="DSC_0090"><img src="http://farm9.staticflickr.com/8072/8311558088_ffc3fd6169_s.jpg" width="50" height="50" alt="DSC_0090"></a>
+				<li>
+					<a href="#" rel="prettyphoto" title="DSC_0090"><img src="themes/images/demo/smallpic.jpg" width="50" height="50" alt="DSC_0090"></a>
 				</li>
-				<li><a href="http://farm9.staticflickr.com/8216/8310537407_bce4092480_b.jpg" rel="prettyphoto" title="DSC_0135"><img src="http://farm9.staticflickr.com/8216/8310537407_bce4092480_s.jpg" width="50" height="50" alt="DSC_0135"></a>
+				<li>
+					<a href="#" rel="prettyphoto" title="DSC_0135"><img src="themes/images/demo/smallpic.jpg" width="50" height="50" alt="DSC_0135"></a>
 				</li>
-				<li><a href="http://farm9.staticflickr.com/8359/8311541748_75a38b1344_b.jpg" rel="prettyphoto" title="DSC_0119"><img src="http://farm9.staticflickr.com/8359/8311541748_75a38b1344_s.jpg" width="50" height="50" alt="DSC_0119"></a>
+				<li>
+					<a href="#" rel="prettyphoto" title="DSC_0119"><img src="themes/images/demo/smallpic.jpg" width="50" height="50" alt="DSC_0119"></a>
 				</li>
-				<li><a href="http://farm9.staticflickr.com/8504/8311544062_b6d5f5507d_b.jpg" rel="prettyphoto" title="DSC_0109"><img src="http://farm9.staticflickr.com/8504/8311544062_b6d5f5507d_s.jpg" width="50" height="50" alt="DSC_0109"></a>
+				<li>
+					<a href="#" rel="prettyphoto" title="DSC_0109"><img src="themes/images/demo/smallpic.jpg" width="50" height="50" alt="DSC_0109"></a>
 				</li>
-				<li><a href="http://farm9.staticflickr.com/8503/8310531789_63be391343_b.jpg" rel="prettyphoto" title="DSC_0154"><img src="http://farm9.staticflickr.com/8503/8310531789_63be391343_s.jpg" width="50" height="50" alt="DSC_0154"></a>
+				<li>
+					<a href="#" rel="prettyphoto" title="DSC_0154"><img src="themes/images/demo/smallpic.jpg" width="50" height="50" alt="DSC_0154"></a>
 				</li>
-				<li><a href="http://farm9.staticflickr.com/8356/8311531844_229256fe86_b.jpg" rel="prettyphoto" title="DSC_0142"><img src="http://farm9.staticflickr.com/8356/8311531844_229256fe86_s.jpg" width="50" height="50" alt="DSC_0142"></a>
+				<li>
+					<a href="#" rel="prettyphoto" title="DSC_0142"><img src="themes/images/demo/smallpic.jpg" width="50" height="50" alt="DSC_0142"></a>
 				</li>
-				<li><a href="http://farm9.staticflickr.com/8224/8310490777_cd2ebecd62_b.jpg" rel="prettyphoto" title="DSC_0120"><img src="http://farm9.staticflickr.com/8224/8310490777_cd2ebecd62_s.jpg" width="50" height="50" alt="DSC_0120"></a>
+				<li>
+					<a href="#" rel="prettyphoto" title="DSC_0120"><img src="themes/images/demo/smallpic.jpg" width="50" height="50" alt="DSC_0120"></a>
 				</li>
-				<li><a href="http://farm9.staticflickr.com/8076/8311583268_243abea7d8_b.jpg" rel="prettyphoto" title="DSC_0138"><img src="http://farm9.staticflickr.com/8076/8311583268_243abea7d8_s.jpg" width="50" height="50" alt="DSC_0138"></a>
+				<li>
+					<a href="#" rel="prettyphoto" title="DSC_0138"><img src="themes/images/demo/smallpic.jpg" width="50" height="50" alt="DSC_0138"></a>
 				</li>
-				<li><a href="http://farm9.staticflickr.com/8080/8310485971_be8307a413_b.jpg" rel="prettyphoto" title="DSC_0131"><img src="http://farm9.staticflickr.com/8080/8310485971_be8307a413_s.jpg" width="50" height="50" alt="DSC_0131"></a>
+				<li>
+					<a href="#" rel="prettyphoto" title="DSC_0131"><img src="themes/images/demo/smallpic.jpg" width="50" height="50" alt="DSC_0131"></a>
 				</li>
 			</ul>
 		</div>
 		<div id="linkcat-2" class="one_fourth widgets widget_links">
-			<h3>Blogroll</h3>
+			<h3>لینک های مفید</h3>
 			<ul class="xoxo blogroll">
-				<li><a href="http://themeforest.net/user/e404">Our ThemeForest profile</a></li>
-				<li><a href="http://e404themes.com">Our website</a></li>
-				<li><a href="http://wordpress.org">WordPress</a></li>
-				<li><a href="http://wordpress.org/extend/plugins/">WordPress Plugins</a></li>
+				<li><a href="#">لینک یک</a></li>
+				<li><a href="#">لینک دو</a></li>
+				<li><a href="#">لینک سه</a></li>
+				<li><a href="#">لینک چهار</a></li>
 			</ul>
 		</div>
-		<div id="e404_twitter-3" class="one_fourth widgets widget_twitter"><h3>Latest Tweets</h3>
+		<div id="e404_twitter-3" class="one_fourth widgets widget_twitter"><h3>اخبار</h3>
 			<ul class="tweets">
-			<li>
-				<a href="http://twitter.com/damopest" class="twitter-user">@damopest</a> We don’t provide a support on Twitter. Please send us a message using our TF profile page - 
-				<a href="http://t.co/mvN0y9hWEr" class="twitter-link">http://t.co/mvN0y9hWEr</a> <span><a href="http://twitter.com/e404themes/status/329964628731912192">on May 2, 2013</a></span>
-			</li>
-			<li>
-				<a href="http://twitter.com/n8k99" class="twitter-user">@n8k99</a> To columns shortcodes? Yes, of course. 
-				<span><a href="http://twitter.com/e404themes/status/327070585597349889">on April 24, 2013</a></span>
-			</li>
-			<li>
-				<a href="http://twitter.com/n8k99" class="twitter-user">@n8k99</a> The column demo page is a regular static page. Columns are created using shortcodes. 
-				<span><a href="http://twitter.com/e404themes/status/327069773462634497">on April 24, 2013</a></span>
-			</li>
+				<li>
+					<a href="#" rel="prettyphoto" title="DSC_0159"><img src="themes/images/demo/smallpic.jpg" width="50" height="50" alt="DSC_0159"></a>
+					<a href="#">لینک یک لینک یک لینک یک لینک یک </a>
+				</li>
+				<br class="clear">
+				<li>
+					<a href="#" rel="prettyphoto" title="DSC_0159"><img src="themes/images/demo/smallpic.jpg" width="50" height="50" alt="DSC_0159"></a>
+					<a href="#">لینک یک</a>
+				</li>
+				<br class="clear">
+				<li>
+					<a href="#" rel="prettyphoto" title="DSC_0159"><img src="themes/images/demo/smallpic.jpg" width="50" height="50" alt="DSC_0159"></a>
+					<a href="#">لینک یک</a>
+				</li>
+				<br class="clear">
 			</ul>
 		</div>
 		<div id="custom_cf7-3" class="one_fourth widgets widget_custom_cf7">
-			<h3>Contact Us</h3>
+			<h3>تماس با ما</h3>
 	    	<div id="cf7_form_box">
 	    	    <div class="wpcf7" id="wpcf7-f853-w1-o1">
-					<form action="/cold/#wpcf7-f853-w1-o1" method="post" class="wpcf7-form">
+					<form action="" method="post" class="wpcf7-form">
 						<div style="display: none;">
 						<input type="hidden" name="_wpcf7" value="853"><br>
 						<input type="hidden" name="_wpcf7_version" value="3.5.2"><br>
@@ -66,13 +81,24 @@
 						<input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f853-w1-o1"><br>
 						<input type="hidden" name="_wpnonce" value="700208ddec">
 						</div>
-						<p>Your Name (required)<br>
-						    <span class="wpcf7-form-control-wrap your-name"><input type="text" name="your-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true"></span> </p>
-						<p>Your Email (required)<br>
-						    <span class="wpcf7-form-control-wrap your-email"><input type="text" name="your-email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true"></span> </p>
-						<p>Your Message<br>
-						    <span class="wpcf7-form-control-wrap your-message"><textarea name="your-message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea"></textarea></span> </p>
-						<p><input type="submit" value="Send Message" class="wpcf7-form-control wpcf7-submit"></p>
+						<p>نام و نام خانوادگی (*)<br>
+						    <span class="wpcf7-form-control-wrap your-name">
+							    <input type="text" name="your-name" value="" size="40" class="rtl wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true">
+							</span>
+						</p>
+						<p>ایمیل شما (*)<br>
+						    <span class="wpcf7-form-control-wrap your-email">
+						    	<input type="text" name="your-email" value="" size="40" class="ltr wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true">
+						    </span>
+						</p>
+						<p>پیام شما<br>
+						    <span class="wpcf7-form-control-wrap your-message">
+						    	<textarea name="your-message" cols="40" rows="10" class="rtl wpcf7-form-control wpcf7-textarea"></textarea>
+						    </span>
+						</p>
+						<p>
+							<input type="submit" value="ارسال پیام" class="wpcf7-form-control wpcf7-submit">
+						</p>
 						<div class="wpcf7-response-output wpcf7-display-none"></div>
 					</form>
 				</div>
