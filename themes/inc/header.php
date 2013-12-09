8dbbc6da61c6480e6bc11fa46a51b3484f3804f1
mojiAm <amjadi.mojtaba@gmail.com>
2013-12-08 18:17:02 -0800

    seprate files

---
+++ themes/inc/header.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="fa-IR"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>SKM</title>
+<!-- <link rel="profile" href="http://gmpg.org/xfn/11"> -->
+<link rel="stylesheet" type="text/css" media="all" href="themes/css/style.css">
+<!-- <link rel="pingback" href="http://e404themes.com/cold/xmlrpc.php">
+<link rel="alternate" type="application/rss+xml" title="Cold Theme » Feed" href="http://e404themes.com/cold/feed/">
+<link rel="alternate" type="application/rss+xml" title="Cold Theme » Comments Feed" href="http://e404themes.com/cold/comments/feed/">
+<link rel="alternate" type="application/rss+xml" title="Cold Theme » Home Comments Feed" href="http://e404themes.com/cold/home/feed/"> -->
+<link rel="stylesheet" id="galleria-css" href="themes/css/galleria.classic.css?ver=3.6.1" type="text/css" media="all">
+<link rel="stylesheet" id="superfish-css" href="themes/css/menu.css?ver=3.6.1" type="text/css" media="all">
+<link rel="stylesheet" id="scrollable-css" href="themes/css/scrollable.css?ver=3.6.1" type="text/css" media="all">
+<link rel="stylesheet" id="nivo-css" href="themes/css/nivo-slider.css?ver=3.6.1" type="text/css" media="all">
+<link rel="stylesheet" id="prettyphoto-css" href="themes/css/prettyphoto.css?ver=3.6.1" type="text/css" media="all">
+<link rel="stylesheet" id="Lato-css" href="themes/css/fonts.css" type="text/css" media="all">
+<link rel="stylesheet" id="contact-form-7-css" href="themes/css/styles.css?ver=3.5.2" type="text/css" media="all">
+
+<script type="text/javascript" src="themes/js/jquery.js?ver=1.10.2"></script>
+<script type="text/javascript" src="themes/js/jquery-migrate.min.js?ver=1.2.1"></script>
+<script type="text/javascript" src="themes/js/preloader.js?ver=3.6.1"></script>
+<script type="text/javascript" src="themes/js/hoverIntent.js?ver=3.6.1"></script>
+<script type="text/javascript" src="themes/js/superfish.js?ver=3.6.1"></script>
+<script type="text/javascript" src="themes/js/scrollable.min.js?ver=3.6.1"></script>
+<script type="text/javascript" src="themes/js/jquery.prettyphoto.js?ver=3.6.1"></script>
+<script type="text/javascript" src="themes/js/prettyphoto_init.js?ver=3.6.1"></script>
+<script type="text/javascript" src="themes/js/comment-reply.min.js?ver=3.6.1"></script>
+
+<script type="text/javascript" src="themes/js/galleria.min.js?ver=3.6.1"></script>
+<script type="text/javascript" src="themes/js/galleria.classic.min.js?ver=3.6.1"></script>
+<script type="text/javascript" src="themes/js/flowplayer.min.js?ver=3.6.1"></script>
+<script type="text/javascript" src="themes/js/jquery.nivo.slider.pack.js?ver=3.6.1"></script>
+<script type="text/javascript" src="themes/js/cold_custom.js?ver=3.6.1"></script>
+<script type="text/javascript" src="themes/js/nivo_slider_init.js?ver=3.6.1"></script>
+<script type="text/javascript" src="themes/js/jquery.form.min.js?ver=3.40.0-2013.08.13"></script>
+
+<!-- <link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://e404themes.com/cold/xmlrpc.php?rsd">
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://e404themes.com/cold/wp-includes/wlwmanifest.xml">  -->
+
+<meta name="generator" content="Mediateq">
+
+<!-- <link rel="canonical" href="http://e404themes.com/cold/"> -->
+
+<link href="themes/css/style_13.css" rel="stylesheet" type="text/css">
+
+<link rel="shortcut icon" href="favicon.ico">
+
+<style type="text/css">
+	h1 { font-family: 'bmitra', tahoma, 'Lato', arial, serif; }
+	h2 { font-family: 'bmitra', tahoma, 'Lato', arial, serif; }
+	h3 { font-family: 'bmitra', tahoma, 'Lato', arial, serif; }
+	h4 { font-family: 'bmitra', tahoma, 'Lato', arial, serif; }
+	h5 { font-family: 'bmitra', tahoma, 'Lato', arial, serif; }
+	h6 { font-family: 'bmitra', tahoma, 'Lato', arial, serif; }
+	.sf-menu li { font-family: 'bmitra',tahoma,'Lato', arial, serif; }
+	.sf-menu li li { font-family: 'bmitra',tahoma,'Lato', arial, serif; }
+</style>
+<style type="text/css">
+	.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}
+</style>
+<style type="text/css">
+	#stylespanel {
+		width: 124px;
+		position: absolute;
+		top: 0px;
+		left: 0px;
+		z-index: 999;
+	}
+	.stylespanel-content {
+		background: #eee;
+		padding: 10px 10px 0;
+		overflow: hidden;
+		display: none;
+	}
+	.stylespanel-content a {
+		float: left;
+		margin: 5px;
+	}
+	.stylespanel-content a:hover {
+		filter: alpha(opacity=80);
+		opacity: 0.8;
+	}
+	.stylespanel-content strong {
+		color: #333;
+	}
+	#stylespanel br {
+		clear: both;
+	}
+	.stylespanel-toggle {
+		background: #eee;
+		padding: 10px 15px;
+		border-radius: 0 0 12px 0;
+		-moz-border-radius: 0 0 12px 0;
+		-webkit-border-radius: 0 0 12px 0;
+	}
+	.stylespanel-toggle a {
+		text-decoration: none;
+		display: block;
+		padding: 5px 0 5px 26px;
+		font-weight: bold;
+		color: #999;
+		text-transform: uppercase;
+		background: url('http://e404themes.com/cold/wp-content/themes/cold/images/cog.png') 0 50% no-repeat;
+	}
+	.stylespanel-toggle a:hover {
+		color: #666;
+	}
+</style>
+</head>
+
+<body>
+<div id="header_wrapper">
+	<div id="header">
+		<div class="rightside" id="logo"><a href="./"><img src="themes/images/demo/logo.png" alt="Saman Kesht Mihan"></a></div>
+		<div id="header_nav" class="leftside">
+			<div id="social_icons" class="leftside">
+				<a href="#" target="_blank" title="Contact"><img src="themes/images/contact.png" alt="Contact"></a>
+				<a href="#" target="_blank" title="RSS"><img src="themes/images/rss.png" alt="RSS"></a>
+				<a href="#" target="_blank" title="Twitter"><img src="themes/images/twitter.png" alt="Twitter"></a>
+				<a href="#" target="_blank" title="Facebook"><img src="themes/images/facebook.png" alt="Facebook"></a>
+				<a href="#" target="_blank" title="Dribbble"><img src="themes/images/dribbble.png" alt="Dribbble"></a>
+			</div>
+			<div id="search" class="leftside">
+				<form action="search.html" method="get">
+					<input type="submit" value="جستجو">
+					<input type="text" class="rtl" name="s" value="جستجو..." onfocus="if (this.value == 'جستجو...') {this.value = '';}" onblur="if (this.value == '') {this.value = 'جستجو...';}">
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+<div id="navigation">
+	<ul id="menu-top-menu" class="sf-menu">
+		<li id="menu-item-427" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-5 current_page_item menu-item-427"><a href="./">صفحه اصلی</a></li>
+		<li id="menu-item-431" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-431"><a href="#">درباره ما</a>
+			<ul class="sub-menu">
+				<li id="menu-item-433" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-433"><a href="#">درباره ما</a></li>
+				<li id="menu-item-448" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-448"><a href="#">درباره ما</a></li>
+				<li id="menu-item-449" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-449"><a href="#">درباره ما</a></li>
+				<li id="menu-item-432" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-432"><a href="#">درباره ما</a></li>
+				<li id="menu-item-827" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-827"><a href="#">درباره ما</a></li>
+				<li id="menu-item-441" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-441"><a href="#">درباره ما</a>
+					<ul class="sub-menu">
+						<li id="menu-item-516" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-516"><a href="#">درباره ما 1</a></li>
+						<li id="menu-item-515" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-515"><a href="#">درباره ما 2</a></li>
+						<li id="menu-item-514" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-514"><a href="#">درباره ما 3</a></li>
+					</ul>
+				</li>
+			</ul>
+		</li>
+		<li id="menu-item-430" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-430"><a href="#">اخبار</a></li>
+		<li id="menu-item-426" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-426"><a href="#">رزومه</a></li>
+		<li id="menu-item-428" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-428"><a href="#">فضای سبز</a></li>
+		<li id="menu-item-429" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-429"><a href="#">گالری تصاویر</a></li>
+		<li id="menu-item-459" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-459"><a href="#">تماس با ما</a></li>
+	</ul>
+	<br class="clear">
+</div>
---
+++ themes/inc/index-content.php
@@ -0,0 +1,176 @@
+<div id="wrapper">
+	<div id="main_wrapper">
+		<div id="featured">
+			<!-- <div class="ribbon"></div>			 -->
+			<div class="slider" id="slider" style="height: 350px;">
+				<img src="themes/images/demo/slide1.jpg" title="اسلاید شماره یک" alt="اسلاید شماره یک">
+				<img src="themes/images/demo/slide2.jpg" title="اسلاید شماره دو" alt="اسلاید شماره دو">
+				<img src="themes/images/demo/slide3.jpg" title="اسلاید شماره سه" alt="اسلاید شماره سه">
+			</div>
+			<script type="text/javascript">
+				var slideParams = []; 
+				slideParams['effect'] = 'random'; 
+				slideParams['slices'] = '10'; 
+				slideParams['animSpeed'] = '600'; 
+				slideParams['pauseTime'] = '4000'; 
+				slideParams['directionNav'] = 1; 
+				slideParams['directionNavHide'] = 1;
+				slideParams['controlNav'] = 1; 
+				slideParams['keyboardNav'] = 1; 
+				slideParams['pauseOnHover'] = 1; 
+				slideParams['manualAdvance'] = false;
+				slideParams['captionOpacity'] = 0.6;
+				slideParams['stopAtEnd'] = 0;
+				slideParams['height'] = 350;
+			</script>
+			<!-- <ul id="featured-boxes">
+				<li class="featured_fourth icon-ruler featured-dark">
+					<div>
+						<h4><a href="http://e404themes.com/cold/shortcodes/">40+ Shortcodes</a></h4><p>Some of them have many different variations.</p></div></li><li class="featured_fourth icon-paint-brush"><div><h4><a href="http://e404themes.com/cold/features/color-variations/">16 Color Variations</a></h4><p><strong>Cold Theme</strong> includes 16 different color variations.</p></div></li><li class="featured_fourth icon-tools featured-dark"><div><h4><a href="http://e404themes.com/cold/features/powerful-admin-panel/">Powerful Panel</a></h4><p>Which allows you to customize your website.</p></div></li><li class="featured_fourth_last icon-help"><div><h4>Customer Support</h4><p>We provide an extensive customer support.</p>
+					</div>
+				</li>
+			</ul> -->
+			<br class="clear">
+		</div>
+		<!-- <div id="intro" class="text-intro">
+			<div class="three_fifth">
+				<p><strong>Welcome to Cold Theme: The ultimate all-in-one template.</strong><br>
+				Create outstanding website or blog in minutes!</p>
+			</div>
+			<div class="two_fifth last center">
+				<a class="big-btn gradient-btn stroke-btn" href="http://themeforest.net/item/cold-theme-powerful-wordpress-theme/236148?ref=e404"><span><img src="http://e404themes.com/cold/wp-content/themes/cold/images/bullets/arrow.png" alt=""> Available on ThemeForest</span></a>
+			</div>
+			<br class="clear">
+		</div> -->
+		<!-- <div class="one_third">
+			<div class="shadow shadow_medium"><a href="http://e404themes.com/cold/features/custom-widgets/"><img src="http://e404themes.com/cold/wp-content/themes/cold/lib/timthumb.php?src=http://e404themes.com/cold/wp-content/uploads/2011/04/widgets.jpg&amp;w=280&amp;h=0&amp;zc=1" alt="" class="border-img"></a></div>
+			<h2>8 Custom Widgets</h2>
+			<p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum.
+			</p>
+		</div>
+		<div class="one_third">
+			<div class="shadow shadow_medium"><a href="http://e404themes.com/cold/contact/"><img src="http://e404themes.com/cold/wp-content/themes/cold/lib/timthumb.php?src=http://e404themes.com/cold/wp-content/uploads/2011/04/mail.jpg&amp;w=280&amp;h=0&amp;zc=1" alt="" class="border-img"></a></div>
+			<h2>Support for Contact Form 7</h2>
+			<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+			</p>
+		</div>
+		<div class="one_third last">
+			<div class="shadow shadow_medium"><a href="http://e404themes.com/cold/shortcodes/dropcaps-boxes-icon-boxes/"><img src="http://e404themes.com/cold/wp-content/themes/cold/lib/timthumb.php?src=http://e404themes.com/cold/wp-content/uploads/2011/04/icons.jpg&amp;w=280&amp;h=0&amp;zc=1" alt="" class="border-img"></a></div>
+			<h2>72 Icon Boxes</h2>
+			<p>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip.
+			</p>
+		</div> -->
+		<br class="clear">
+		<!-- <hr class="dotted"> -->
+		<div class="one_third">
+			<h2 class="head-icon"><img src="themes/images/sign.png" class="icon" alt="Slide Show"><a href="http://e404themes.com/cold/features/">هدر یک</a><span>هدر یک</span></h2>
+			<p>توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... </p>
+			<p class="more"><a href="#">اطلاعات تکمیلی</a></p>
+		</div>
+		<div class="one_third">
+			<h2 class="head-icon"><img src="themes/images/sign.png" class="icon" alt="Google Web Fonts"><a href="http://www.google.com/webfonts">هدر یک</a><span>هدر یک</span></h2>
+			<p>توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... </p>
+			<p class="more"><a href="#">اطلاعات تکمیلی</a></p>
+		</div>
+		<div class="one_third last">
+			<h2 class="head-icon"><img src="themes/images/sign.png" class="icon" alt="PrettyPhoto"><a href="http://e404themes.com/cold/shortcodes/">هدر یک</a><span>هدر یک</span></h2>
+			<p>توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... </p>
+			<p class="more"><a href="#">اطلاعات تکمیلی</a></p>
+		</div>
+		<br class="clear">
+		<div class="fancy_list">
+			<div class="one_fifth fancy_title">
+				<h3>کارهای اخیر</h3>
+				<p><a href="#">مشاهده تمام موارد</a></p>
+			</div>
+			<div class="one_fifth fancy_list_item">
+				<div class="shadow shadow_vsmall aligncenter shadow_center">
+					<a href="#">
+						<img src="themes/images/demo/smallpic.jpg" alt="" title="" class="border-img">
+					</a>
+				</div>
+				<h4>
+					<a href="#">هدر اول</a>
+				</h4>
+			</div>
+			<div class="one_fifth fancy_list_item">
+				<div class="shadow shadow_vsmall aligncenter shadow_center">
+					<a href="#">
+						<img src="themes/images/demo/smallpic.jpg" alt="" title="" class="border-img">
+					</a>
+				</div>
+				<h4>
+					<a href="#">هدر دوم</a>
+				</h4>
+			</div>
+			<div class="one_fifth fancy_list_item">
+				<div class="shadow shadow_vsmall aligncenter shadow_center">
+					<a href="">
+						<img src="themes/images/demo/smallpic.jpg" alt="" title="" class="border-img">
+					</a>
+				</div>
+				<h4>
+					<a href="">هدر سوم</a>
+				</h4>
+			</div>
+			<div class="one_fifth last fancy_list_item">
+				<div class="shadow shadow_vsmall aligncenter shadow_center">
+					<a href="">
+						<img src="themes/images/demo/smallpic.jpg" alt="" title="" class="border-img">
+					</a>
+				</div>
+				<h4>
+					<a href="">هدر چهارم</a>
+				</h4>
+			</div>
+			<br class="clear">
+		</div>
+		<div class="fancy_list">
+			<div class="one_fifth fancy_title">
+				<h3>اخبار</h3>
+				<p><a href="#">مشاهده تمام موارد</a></p>
+			</div>
+			<div class="one_fifth fancy_list_item">
+				<div class="shadow shadow_vsmall">
+					<a href="">
+						<img src="themes/images/demo/smallpic.jpg" alt="" title="" class="border-img aligncenter">
+					</a>
+				</div>
+				<h4>
+					<a href="">هدر اول</a>
+				</h4>
+			</div>
+			<div class="one_fifth fancy_list_item">
+				<div class="shadow shadow_vsmall">
+					<a href="">
+						<img src="themes/images/demo/smallpic.jpg" alt="y" title="" class="border-img aligncenter">
+					</a>
+				</div>
+				<h4>
+					<a href="">هدر دوم</a>
+				</h4>
+			</div>
+			<div class="one_fifth fancy_list_item">
+				<div class="shadow shadow_vsmall">
+					<a href="">
+						<img src="themes/images/demo/smallpic.jpg" alt="" title="" class="border-img aligncenter">
+					</a>
+				</div>
+				<h4>
+					<a href="">هدر سوم</a>
+				</h4>
+			</div>
+			<div class="one_fifth last fancy_list_item">
+				<div class="shadow shadow_vsmall">
+					<a href="">
+						<img src="themes/images/demo/smallpic.jpg" alt="" title="" class="border-img aligncenter">
+					</a>
+				</div>
+				<h4>
+					<a href="">هدر چهارم</a>
+				</h4>
+			</div>
+			<br class="clear">
+		</div>
+	</div>
+</div>
