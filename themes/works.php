d32016ebd7a9756dcf09568069a3f809cca099a5
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-12-10 10:14:14 +0330

    fix single resume page

--- themes/works.php
+++ themes/works.php
@@ -7,7 +7,57 @@ $html=<<<cd
 				<a href="./">صفحه اصلی</a> <span>›</span>
 				<a>رزومه</a>
 			</div>
-	
+			<div class="portfolio portfolio-columns">
+				<div class="one_third portfolio-item" id="post-294">
+					<div class="shadow shadow_medium">
+						<a href="#" class="zoom" rel="prettyPhoto" title="">
+							<img src="themes/images/demo/slide1.jpg" alt="Cold Default" class="border-img" style="width:280px;height:157px;">
+						</a>
+					</div>
+					<h3>
+						<a href="#" title="Cold Default">رزومه اول</a>
+					</h3>
+					<p>توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... </p>
+					<p class="more"><a href="#">ادامه رزومه</a></p>
+				</div>
+				<div class="one_third portfolio-item" id="post-294">
+					<div class="shadow shadow_medium">
+						<a href="#" class="zoom" rel="prettyPhoto" title="">
+							<img src="themes/images/demo/slide1.jpg" alt="Cold Default" class="border-img" style="width:280px;height:157px;">
+						</a>
+					</div>
+					<h3>
+						<a href="#" title="Cold Default">رزومه اول</a>
+					</h3>
+					<p>توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... </p>
+					<p class="more"><a href="#">ادامه رزومه</a></p>
+				</div>
+				<div class="one_third portfolio-item last" id="post-294">
+					<div class="shadow shadow_medium">
+						<a href="#" class="zoom" rel="prettyPhoto" title="">
+							<img src="themes/images/demo/slide1.jpg" alt="Cold Default" class="border-img" style="width:280px;height:157px;">
+						</a>
+					</div>
+					<h3>
+						<a href="#" title="Cold Default">رزومه اول</a>
+					</h3>
+					<p>توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... </p>
+					<p class="more"><a href="#">ادامه رزومه</a></p>
+				</div>
+				<div class="one_third portfolio-item" id="post-294">
+					<div class="shadow shadow_medium">
+						<a href="#" class="zoom" rel="prettyPhoto" title="">
+							<img src="themes/images/demo/slide1.jpg" alt="Cold Default" class="border-img" style="width:280px;height:157px;">
+						</a>
+					</div>
+					<h3>
+						<a href="#" title="Cold Default">رزومه اول</a>
+					</h3>
+					<p>توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... توضیحات... </p>
+					<p class="more"><a href="#">ادامه رزومه</a></p>
+				</div>
+				<br class="clear">			
+			</div>
 		</div>
 	</div>
 cd;
