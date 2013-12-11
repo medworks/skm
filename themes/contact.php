8651103473e08eb18af32e3e281cb4cbfb2301fd
mojiAm <amjadi.mojtaba@gmail.com>
2013-12-11 12:21:56 -0800

    add contact page

---
+++ themes/contact.php
@@ -0,0 +1,19 @@
+<?php
+
+$html=<<<cd
+	<div id="wrapper">
+		<div id="main_wrapper">
+			<div id="breadcrumb">
+				<a href="./">صفحه اصلی</a> <span>›</span>
+				<a>تماس با ما</a>
+			</div>
+			<div id="intro" class="text-intro">
+				<h1>تماس با ما</h1>
+			</div>
+			
+		</div>
+	</div>
+cd;
+
+return $html;
+?>
